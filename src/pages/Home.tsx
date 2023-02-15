b310cffca4d49620c78e14bc8a3722efbec136f0
morningYXX <1145640337@qq.com>
2023-02-15 18:33:27 +0800

    首页样式

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,5 +11,42 @@ export default () => {
       <img className='icon' src={svg_dc} alt="svg_dc" onClick={() => window.open(frendslinkConfig.discord)} />
       <img className='icon' src={svg_twitter} alt="svg_twitter" onClick={() => window.open(frendslinkConfig.twitter)} />
     </div>
+    <p className='subtitle'>The first 10k NFT collection on Bitcoin</p>
+    <button className='btn'>Verified Listings</button>
+    <p className='minte'>10000 / 10000 minted!</p>
+    <div className='explain'>
+      <p>Bitcoin Punks are the first byte-perfect uploads of the <a href="https://www.larvalabs.com/cryptopunks">original Ethereum CryptoPunks</a> onto the Bitcoin Blockchain using <a href="https://ordinals.com/">Ordinals</a>.</p>
+      <p>To make this site, we are checking the hash of every image uploaded to Ordinals and comparing it against the original 10k punk images. The links to Bitcoin Punks are the first-seen inscriptions (lowest ID) that contain these hashes on Ordinals.</p>
+    </div>
+    <div className='filters'>
+      <div className='filter-item'>
+        <span className='lable'>Short By:</span>
+        <select className='select'>
+          <option value="1">Randam</option>
+        </select>
+      </div>
+      <input type="text" placeholder='Search mfers ID' />
+      <div className='filter-item'>
+        <span className='lable'>Show IDs</span>
+        <span className='my-switch'></span>
+      </div>
+    </div>
+    <div className='icons-grid'>
+      <div className='grid'>1</div>
+      <div className='grid'>2</div>
+      <div className='grid'>3</div>
+      <div className='grid'>3</div>
+      <div className='grid'>3</div>
+      <div className='grid'>1</div>
+      <div className='grid'>2</div>
+      <div className='grid'>3</div>
+      <div className='grid'>3</div>
+      <div className='grid'>3</div>
+      <div className='grid'>1</div>
+      <div className='grid'>2</div>
+      <div className='grid'>3</div>
+      <div className='grid'>3</div>
+      <div className='grid'>3</div>
+    </div>
   </div>
 }
